78a6820f6711d5715bdafec5bfecbc8337fb1876
Martin Scheiber <martin.scheiber@aau.at>
2021-05-03 11:49:20 +0200

    [TEST] Changes to configs after initial test.

--- watchdog_bringup/scripts/drivers/ids_check.sh
+++ watchdog_bringup/scripts/drivers/ids_check.sh
@@ -3,6 +3,7 @@
 # see https://tldp.org/LDP/abs/html/exitcodes.html for further information
 
 echo "[BASH] checking sensor - FAILURE"
+exit 1
 
 IDS_STATUS="$(systemctl status ueyeusbdrc)"
 echo "$IDS_STATUS"
@@ -10,7 +11,7 @@ echo "$IDS_STATUS"
 case $IDS_STATUS in
   "")
     echo "[BASH - IDS] Service cannot be found"
-    exit 1
+    exit 0
     ;;
 esac
 
