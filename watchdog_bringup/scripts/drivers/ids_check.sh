26d4eadf08663de5f5fdfb4c16c5caf3508fb6da
Martin Scheiber <martin.scheiber@aau.at>
2021-05-06 15:48:38 +0200

    [ADD] Updated scripts for driver start/check.
    Co-Authored-By: Christian Brommer <christian.brommer@aau.at>

--- watchdog_bringup/scripts/drivers/ids_check.sh
+++ watchdog_bringup/scripts/drivers/ids_check.sh
@@ -1,18 +1,50 @@
 #!/bin/bash
+# Copyright (C) 2021 Martin Scheiber, Control of Networked Systems, University of Klagenfurt, Austria.
+#
+# All rights reserved.
+#
+# This software is licensed under the terms of the BSD-2-Clause-License with
+# no commercial use allowed, the full terms of which are made available
+# in the LICENSE file. No license in patents is granted.
+#
+# You can contact the author at <martin.scheiber@ieee.org>
+
 # this bash script adheres to linux exit codes
 # see https://tldp.org/LDP/abs/html/exitcodes.html for further information
 
 echo "[BASH - IDS] checking sensor - IDS Camera"
-exit 1
 
 IDS_STATUS="$(systemctl status ueyeusbdrc)"
 echo "[BASH - IDS] status: ${IDS_STATUS}"
 
 case $IDS_STATUS in
   "")
+    # service cannot be found
     echo "[BASH - IDS] Service cannot be found"
     exit 1
     ;;
+  *)
+    # check if service is active
+    IS_ACTIVE=$(echo "$IDS_STATUS" | grep "Active: active (running)")
+    if [[ -z "${IS_ACTIVE}" ]]; then
+      # service is not active
+      echo "[BASH - IDS] service is not active"
+      exit 1
+    else
+      # check if status contains fail pattern
+      HAS_FAILED=$(echo "$IDS_STATUS" | grep "0xffffffff")
+      if [[ -z "${HAS_FAILED}" ]]; then
+        # when empty driver is ok
+        echo "[BASH - IDS] camera driver is OK"
+        exit 0
+      else
+        # driver loading has failed
+        echo "[BASH - IDS] driver start has failed"
+        exit 1
+      fi
+    fi
+    ;;
 esac
 
-exit 0
+# in case something went wrong
+exit 1
--- watchdog_bringup/scripts/drivers/rs_restart.sh
+++ watchdog_bringup/scripts/drivers/rs_restart.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
+# Copyright (C) 2021 Martin Scheiber and Christian Brommer,
+# Control of Networked Systems, University of Klagenfurt, Austria.
+#
+# All rights reserved.
+#
+# This software is licensed under the terms of the BSD-2-Clause-License with
+# no commercial use allowed, the full terms of which are made available
+# in the LICENSE file. No license in patents is granted.
+#
+# You can contact the authors at <martin.scheiber@ieee.org>
+# and <christian.brommer@ieee.org>
+
 # this bash script adheres to linux exit codes
 # see https://tldp.org/LDP/abs/html/exitcodes.html for further information
 
-# connect via ssh to pi with realsense
-# TODO(scm)
-
 # restart realsense hub
-#   install uhubctl then I put in /etc/rc.local to power cycle usb hub:
-#   uhubctl -a cycle -l 1 -p 1-4
-#   uhubctl -a cycle -l 2 -p 1-4
-sudo uhubctl -h
+ssh core@10.42.0.102 'sudo uhubctl -a off --delay 2 -e -R -l2 -p1'
 
 exit 0
